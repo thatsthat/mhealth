d8d2264056d85b4a3ba9e387e33d76bb1e510e44
Aram <aram.anto@gmail.com>
2019-12-05 16:43:29 +0000

    Added support to write results into a text file, google function

--- google_search.js
+++ google_search.js
@@ -1,9 +1,20 @@
 var gplay = require('google-play-scraper');
+var fs = require('fs');
 
-gplay.search({
-    term: "hayfever",
-    lang: "en",
-    country: "us",
-    price: "free",
+opts = {
+    term: 'hayfever',
+    lang: 'en',
+    country: 'us',
+    price: 'x free',
     num: 3
-}).then(console.log, console.log);
+};
+
+gplay.search(opts)
+    .then( (results, err) => {
+	fs.writeFile('google_results.txt', JSON.stringify(results), (err) => {
+	   // throws an error, you could also catch it here
+	   if (err) throw err;
+	   // success case, the file was saved
+	   console.log('List of apps saved!');
+       });
+   }).catch();
