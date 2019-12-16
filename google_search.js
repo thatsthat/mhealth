467c1a41036f206be9ce6f9ddd1f79b46900aabb
Aram <aram.anto@gmail.com>
2019-12-16 13:04:55 +0100

    updated google function with same interation over countries and search terms

--- google_search.js
+++ google_search.js
@@ -1,32 +1,52 @@
-var gplay = require('google-play-scraper');
+var store = require('google-play-scraper');
 var fs = require('fs');
 
-opts = {
-    term: process.argv[2],     // Search expression
-    lang: 'en',                // App language
-    country: process.argv[3],  // Gplay store country 2 letter code
-    num: process.argv[4],      // Number of search results, max is 250
-    price: 'free:',            // all, free, paid
-    fullDetail: false,         // if true an extra request is made for each app
-    throttle: 10	       // Throttle to X requests per second
-};
-
-var search_term = opts.term.split(' ').join('_');
-var file_name = ['results/google_results_'+search_term+'_'+opts.country+'.txt'];
-
-gplay.search(opts)
-    .then( (results, err) => {
-	results = results.map(function(res) {
-	    return { title: res.title,
-		     appId: res.appId,
-		     url: res.url,
-		     // summary: res.summary,
-		     country: opts.country,
-		     term: opts.term
-		   };
+justDoIt()
+    .then( (resul, err) => {
+	const file_name = ['results/google_results_'+process.argv[2]+'.txt'];
+	fs.writeFile(file_name.toString(), JSON.stringify(resul, null, 2), (err) => {
+	    if (err) throw err;
+	    console.log('iOS apps saved!');
 	});
-	fs.writeFile(file_name.toString(), JSON.stringify(results, null, 2), (err) => {
-	   if (err) throw err;
-	   console.log('Android apps saved!');
-        });
-    }).catch();
+    }).catch();  
+
+async function justDoIt(){
+    var terms = ['hay fever', 'hayfever', 'asthma', 'allergic rhinitis'];
+    var countries = ['us', 'au'];
+    var resu = [];
+
+    for (let i = 0; i < terms.length; i++) {
+	for (let j = 0; j < countries.length; j++) { 
+	    const results2 = await scrapeApps(terms[i], countries[j], process.argv[2])
+	    resu = resu.concat(pruneResults(results2, terms[i], countries[j]));
+	    console.log(i);
+	}
+    }
+    return resu
+}
+
+function scrapeApps(terms, countr, nums) { 
+    opts = {
+	term: terms,     // Search expression
+	lang: 'en',                // App language
+	country: countr,  // Gplay store country 2 letter code
+	num: nums,      // Number of search results, max is 250
+	price: 'free:',            // all, free, paid
+	fullDetail: false,         // if true an extra request is made for each app
+	throttle: 10	       // Throttle to X requests per second
+    };
+    return res = store.search(opts)
+}
+
+function pruneResults(fullResults, searchTerms, country) {
+    const prunedResults = fullResults.map(function(res) {
+	return { title: res.title,
+		 appId: res.appId,
+		 url: res.url,
+		 // summary: res.summary,
+		 terms: searchTerms,
+		 countries: country
+	       };
+    });
+    return prunedResults
+}			 
