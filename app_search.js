c8153bfd34b51b6bc3fb86d61a9475881439b003
eltriplet <aram.anto@gmail.com>
2021-05-04 09:52:30 +0000

    define structure for custom keywords sets per country

--- app_search.js
+++ app_search.js
@@ -4,17 +4,14 @@ var fs = require("fs");
 var simil = require("string-similarity");
 const parse2csv = require("json2csv");
 const SQLite = require("better-sqlite3");
-const numApps = 3; //process.argv[2];
+const numApps = 10; //process.argv[2];
 
-main()
-  .then((resul, err) => {
-    const file_name = ["results/App_german_Results_" + numApps + "sql.csv"];
-    const resFields = Object.keys(resul[0]);
-    // Connect to sqlite db
-    let db = new SQLite('results/apps.sqlite');
-    // Create apps table with all properties
-    db.prepare('DROP TABLE IF EXISTS apps').run();
-    db.prepare(`CREATE TABLE apps (appId TEXT,
+// Connect to sqlite db
+const db = new SQLite('results/apps.sqlite');
+
+// Create apps table with all properties
+db.prepare('DROP TABLE IF EXISTS apps').run();
+db.prepare(`CREATE TABLE apps (appId TEXT,
       title TEXT,
       url  TEXT,
       genre TEXT,
@@ -31,41 +28,23 @@ main()
       dev_a TEXT,
       dev_g TEXT,
       updated TEXT);`)
-      .run();
-    db.pragma("synchronous = 1");
-    db.pragma("journal_mode = wal");
-    // Insert all apps from json array into sql table
-    resul.map((res) => {
-      db.prepare(`INSERT OR REPLACE INTO apps VALUES (
-      @appId,
-      @title,
-      @url,
-      @genre,
-      @terms,
-      @countries,
-      @store,
-      @description,
-      @summary,
-      @installs,
-      @score_a,
-      @ratings_a,
-      @score_g,
-      @ratings_g,
-      @dev_a,
-      @dev_g,
-      @updated);`).run(res);;
-    });
+  .run();
+db.pragma("synchronous = 1");
+db.pragma("journal_mode = wal");
+
+main(db)
+  .then((resul, err) => {
     // Read apps from DB
-    resul2 = db.prepare("SELECT * FROM apps").all();
-
+    resulSQL = db.prepare("SELECT * FROM apps").all();
+    // Export apps array to CSV file
+    const file_name = ["results/App_german_Results_" + numApps + ".csv"];
+    const resFields = Object.keys(resul[0]);
     const opts = { fields: resFields, withBOM: true };
-    const resul_csv = parse2csv.parse(resul2, opts);
+    const resul_csv_sql = parse2csv.parse(resulSQL, opts);
     fs.writeFile(file_name.toString(), resul_csv, (err) => {
-      // fs.writeFile(file_name.toString(), JSON.stringify(resul_csv, null, 2), (err) => {
       if (err) throw err;
-      console.log("Apps saved!");
+      console.log("Apps saved from SQL!");
     });
-
     db.close((err) => {
       if (err) { return console.error(err.message); }
       console.log('Close the database connection.');
@@ -73,12 +52,12 @@ main()
   })
   .catch();
 
-async function main() {
+async function main(db) {
   var terms = [
-    "hay fever"];
-  /*  "hayfever",
-      "asthma",
-      "rhinitis",
+    "hay fever",
+    "hayfever",
+    "asthma"];
+  /*  "rhinitis",
       "allergic rhinitis",
       "allergische schnupfen",
       "allergische rhinitis",
@@ -87,30 +66,36 @@ async function main() {
       "heufieber",
     ];
   */
-  var countries = ["de"];
-  var resAll = [];
+  var countries = ['us', 'gb', 'au'];
+  //var resAll = [];
 
   for (let i = 0; i < terms.length; i++) {
     for (let j = 0; j < countries.length; j++) {
       console.log(`Searching ${terms[i]} in ${countries[j]} Google Play Store`);
       const resGoogle = await gStore.gScrape(terms[i], countries[j], numApps);
-      resAll = resAll.concat(resGoogle);
+      //console.log(resGoogle[0]);
+      saveDB(resGoogle, db);
+      //resAll = resAll.concat(resGoogle);
       console.log(`Searching ${terms[i]} in ${countries[j]} iOS App Store`);
       const resApple = await aStore.aScrape(terms[i], countries[j], numApps);
-      resAll = resAll.concat(resApple);
+      saveDB(resApple, db);
+      //resAll = resAll.concat(resApple);
     }
   }
+  // read list of apps from DB
+  let resAll = db.prepare("SELECT * FROM apps").all();
   resAll = resAll.filter(Boolean); // remove 'undefined' apps
   let firstRound = mergeDups(resAll);
   let secRound = await findMissing(firstRound);
   let secondRound = mergeDups(secRound);
-
-  return postProc(secondRound);
+  //let finalRes = postProc(secondRound)
+  return secondRound;
 }
 
 function postProc(fullRes) {
   // Calculate average of scores and sum ratings
   return fullRes.map((res) => {
+
     if (res.score_a.length == 1) res.score_a = res.score_a[0];
     else {
       let nonzScores = res.score_a.filter((elem) => elem > 0);
@@ -156,9 +141,13 @@ function mergeDups(fullRes) {
         ac.appId = ac.appId.concat([", " + cv.appId]);
       if (!ac.genre.includes(cv.genre))
         ac.genre = ac.genre.concat([", " + cv.genre]);
+      /* FIXME
       if (!ac.score_a.includes(cv.score_a[0])) ac.score_a.push(cv.score_a[0]);
-      if (!ac.ratings_a.includes(cv.ratings_a[0]))
-        ac.ratings_a.push(cv.ratings_a[0]);
+      if (!ac.ratings_a.includes(cv.ratings_a[0])) ac.ratings_a.push(cv.ratings_a[0]);
+      */
+      ac.score_a = cv.score_a;
+      ac.ratings_a = cv.ratings_a;
+      // /FIXME
       if (!ac.summary && cv.summary) ac.summary = cv.summary;
       if (!ac.score_g && cv.score_g) ac.score_g = cv.score_g;
       if (!ac.ratings_g && cv.ratings_g) ac.ratings_g = cv.ratings_g;
@@ -206,4 +195,120 @@ async function findMissing(fullRes) {
     }
   }
   return (resAll = fullRes.concat(missingApps));
+}
+
+function saveDB(resul, dataBase) {
+  // Insert all apps from json array into sql table
+  resul.map((res) => {
+    dataBase.prepare(`INSERT OR REPLACE INTO apps VALUES (
+      @appId,
+      @title,
+      @url,
+      @genre,
+      @terms,
+      @countries,
+      @store,
+      @description,
+      @summary,
+      @installs,
+      @score_a,
+      @ratings_a,
+      @score_g,
+      @ratings_g,
+      @dev_a,
+      @dev_g,
+      @updated);`).run(res);;
+  });
+}
+
+function prepareInput(countries) {
+
+  // Define set of keywords for each language
+  const engKW = ['urticaria', 'hive', 'hives', 'wheal', 'weal',
+    'angio-edema', 'angioedema', 'itch', 'pruritus'];
+  const spanWK = ['urticaria', 'roncha', 'ronchas', 'habón',
+    'habon', 'angioedema', 'picor', 'prurito'];
+  const gerKW = ['urtikaria', 'nesselsucht', 'nesselfieber',
+    'quaddel', 'angioödem', 'juckreiz', 'pruritus'];
+
+  // Define each language 
+  const langs = {
+    english: {
+      opts: {
+        apple: { lang: "en-us" },
+        google: { lang: "en" }
+      },
+      keyWords: engKW
+    },
+    spanish: {
+      opts: {
+        apple: { lang: "es" },
+        google: { lang: "es" }
+      },
+      keyWords: spanKW
+    },
+    german: {
+      opts: {
+        apple: { lang: "de" },
+        google: { lang: "de" }
+      },
+      keyWords: gerKW
+    }
+  }
+
+  // Define set of languages for each country
+  const countries = {
+    us: {
+      country: 'us';
+      languages: [langs.english, langs.spanish];
+    },
+    ca: {
+      country: 'ca';
+      languages: [langs.english];
+    },
+    uk: {
+      country: 'uk';
+      languages: [langs.english];
+    },
+    au: {
+      country: 'au';
+      languages: [langs.english]
+    },
+    es: {
+      country: 'es';
+      languages: [langs.spanish]
+    },
+    ec: {
+      country: 'ec';
+      languages: [langs.spanish]
+    },
+    ar: {
+      country: 'ar';
+      languages: [langs.spanish]
+    },
+    co: {
+      country: 'co';
+      languages: [langs.spanish]
+    },
+    cl: {
+      country: 'cl';
+      languages: [langs.spanish]
+    },
+    mx: {
+      country: 'mx';
+      languages: [langs.spanish]
+    },
+    de: {
+      country: 'de';
+      languages: [langs.german, langs.english]
+    },
+    ch: {
+      country: 'ch';
+      languages: [langs.german, langs.english]
+    },
+    at: {
+      country: 'at';
+      languages: [langs.german, langs.english]
+    }
+  }
 }
